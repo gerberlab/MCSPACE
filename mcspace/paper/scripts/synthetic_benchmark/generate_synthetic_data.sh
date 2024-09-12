1b644c742eac281869495b4a126e74f218a326cc
garyuppal <gary.s.uppal@gmail.com>
2024-09-12 16:39:20 -0400

    update gen data script

--- mcspace/paper/scripts/synthetic_benchmark/generate_synthetic_data.sh
+++ mcspace/paper/scripts/synthetic_benchmark/generate_synthetic_data.sh
@@ -2,4 +2,4 @@
 set -e
 source ../settings.sh
 
-python ./helpers/synthetic/gen_semisyn_data.py -d ${PROJECT_DIR} -o ${OUTPUT_DIR}
+python ./helpers/synthetic/gen_semisyn_data.py -d "${PROJECT_DIR}" -o "${OUTPUT_DIR}"
