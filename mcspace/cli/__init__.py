55191f361fb141f42ba4707a4f365a32d6f13a85
garyuppal <gary.s.uppal@gmail.com>
2024-11-22 20:21:12 -0500

    add data processing CLI

--- mcspace/cli/__init__.py
+++ mcspace/cli/__init__.py
@@ -1,10 +1,11 @@
 from .base import dispatch
-
+from .process_data import ProcessDataCLI
 
 def main():
     print("Hello world!!")
     # ================== Mapping of subcommands to cli modules.
     clis = [
+        ProcessDataCLI(subcommand="parse")
     ]
 
     dispatch({
